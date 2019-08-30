afa0a6b843daa48884658a388acecd1e8edbd516
genie.junior <40048339+jinhee1875@users.noreply.github.com>
2019-08-30 16:41:05 +0900

    python_basic_190830

---
+++ test3.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:36 2019
+
+@author: jinhe
+"""
+#%%
+print("안녕")
+
+
+#%%
+print("안녕" + "하세요")
+
+#%%
+print("안녕", "하세요")
+
+#%%
+print('1+4')
+print(1+4)
+print(1/4)
+print('5'/3)
+
+#%%
+print('5' + '3')
+print('5' * 3)
+
+a = '5'
+print(a)
+b = '김치'
+milk = '물'
+_car = 153
+print(milk)
+
+a = 33
+print(a)
+
+#%%
